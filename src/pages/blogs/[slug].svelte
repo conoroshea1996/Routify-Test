d167abe22bf30a854e60f9cbfe31137a88fecd57
conor o shea <conoshea12@gmail.com>
2021-03-05 16:48:29 +0000

    - add select list - update blog styles

--- src/pages/blogs/[slug].svelte
+++ src/pages/blogs/[slug].svelte
@@ -1,5 +1,5 @@
 <script>
-  import { url, layout, page,goto } from '@roxi/routify'
+  import { url, layout} from '@roxi/routify'
   export let slug;  
 
   import Marker from "marked"
@@ -13,8 +13,8 @@
     // We should redirect to _fallback page
     console.warn(`Could not find child matching slug of ${slug}`)
   }
-
   const blog = post.meta.frontmatter;
+
 </script>
 <style global>
  .main{
@@ -24,11 +24,28 @@
  .main > * {
    text-align: center;
  }
+
+ .img-container{
+    display: flex;
+    justify-content: center;
+ }
 </style>
-<h1> {blog.slug} </h1>
 
 <h4> {blog.title} </h4>
+
+<h5> Categories</h5>
+{#each blog.Categories as cat }
+  <p style="text-align:center">{cat}</p>
+{/each}
+
+
+<div class="img-container">
+  <img style="margin:auto;" width="350px" src={blog.thumbnail} >
+</div>
 
 <div class="main">
  {@html Marker(blog.main)}
 </div>
+
+
+<h4> written by {blog.author} </h4>
